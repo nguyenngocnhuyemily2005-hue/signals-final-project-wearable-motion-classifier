23c4fe4bdf4e188e74360e87b0a273e5abce0d59
nguyenngocnhuyemily2005-hue <nguyenngocnhuyemily2005@gmail.com>
2026-05-12 20:57:14 +0700

    Setup ESP32-S3 PlatformIO project and prepare MPU6050 integration

--- wearable-motion-classifier/src/main.cpp
+++ wearable-motion-classifier/src/main.cpp
@@ -1,13 +1,48 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+
+Adafruit_MPU6050 mpu;
 
 void setup() {
     Serial.begin(115200);
     delay(2000);
 
-    Serial.println("ESP32 STARTED");
+    Wire.begin(8, 9);
+
+    if (!mpu.begin()) {
+        Serial.println("MPU6050 not found!");
+        while (1) {
+            delay(10);
+        }
+    }
+
+    Serial.println("MPU6050 connected!");
 }
 
 void loop() {
-    Serial.println("hello esp32");
-    delay(1000);
+    sensors_event_t a, g, temp;
+
+    mpu.getEvent(&a, &g, &temp);
+
+    Serial.print("AX: ");
+    Serial.print(a.acceleration.x);
+
+    Serial.print(" AY: ");
+    Serial.print(a.acceleration.y);
+
+    Serial.print(" AZ: ");
+    Serial.print(a.acceleration.z);
+
+    Serial.print(" GX: ");
+    Serial.print(g.gyro.x);
+
+    Serial.print(" GY: ");
+    Serial.print(g.gyro.y);
+
+    Serial.print(" GZ: ");
+    Serial.println(g.gyro.z);
+
+    delay(200);
 }
